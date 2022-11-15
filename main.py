9f47ca9040679042a4b2f40be31f1d2215f7cb8e
ImorTaLyTI <sumbadze.shoto22@gtu.ge>
2022-11-15 23:08:47 +0400

    init

---
+++ main.py
@@ -0,0 +1,36 @@
+import tkinter
+from tkinter import *
+
+"""
+class Main:
+    def static hello_world(self):
+        print("hello world")
+        
+"""
+
+def subtract():
+
+
+def add(i):
+    i += 1
+
+
+
+i = 0
+i = i + 1
+
+window = Tk()
+
+window.title("Shoto app")
+
+window.geometry('800x200')
+
+lbl = Label(window, text="Hello")
+
+lbl.grid(column=0, row=1)
+
+btn = Button(window, text="Click Me", command=shoto)
+
+btn.grid(column=1, row=1)
+
+window.mainloop()
